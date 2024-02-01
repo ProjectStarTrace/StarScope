6dd6728044e54bc503430a5cdea50102fa51e0f9
CalebJTipton <caleb@tipton.cloud>
2024-02-01 09:36:30 -0600

    Login state working and sign in and sign out behavior added

--- src/Header.js
+++ src/Header.js
@@ -4,14 +4,43 @@ import { collection, getDocs } from 'firebase/firestore';
 import { db } from './Firebase'; // Adjust this import according to your project structure
 import logo from './assets/StarTraceLogo_white.svg'; // Ensure this path is correct
 import './Header.css'; // CSS file for styling the header
+import { useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 
 function Header() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const auth = getAuth();
+
+
     const [activeUsers, setActiveUsers] = useState(0);
     const [activeScouts, setActiveScouts] = useState(0);
+
+    const navigate = useNavigate();
+
+    const goToLogin = () => navigate('/login');
+    const goToSignup = () => navigate('/signup');
 
     useEffect(() => {
         fetchActiveUsersAndScouts();
     }, []);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setIsLoggedIn(!!user);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, [auth]);
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            navigate('/home');
+        }).catch((error) => {
+            console.error('Logout error', error);
+        });
+    };
 
     const fetchActiveUsersAndScouts = async () => {
         // Fetch total number of users
@@ -38,8 +67,17 @@ function Header() {
                 <p>Active Scouts: {activeScouts}</p>
             </div>
             <div className="header-buttons">
-        <button className="sign-in-btn">Sign In</button>
-        <button className="sign-up-btn">Sign Up</button>
+        {isLoggedIn ? (
+                <>
+                    <button className="auth-button" onClick={() => navigate('/profile')}>Profile</button>
+                    <button className="auth-button" onClick={handleLogout}>Log Out</button>
+                </>
+            ) : (
+                <>
+                    <button className="sign-in-btn" onClick={() => navigate('/login')}>Login</button>
+                    <button className="sign-up-btn" onClick={() => navigate('/signup')}>Sign Up</button>
+                </>
+            )}
       </div>
         </div>
     );
